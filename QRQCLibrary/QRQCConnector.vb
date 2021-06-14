a574ecd977c94d0fb29ebddcc168c96be321757e
menymp <lmmoralespe@hotmail.com>
2021-06-13 22:20:37 -0600

    Agregar archivos de proyecto.

---
+++ QRQCLibrary/QRQCConnector.vb
@@ -0,0 +1,97 @@
+﻿
+Imports System.IO
+
+''' <summary>
+''' Conector de QRQC para la administracion de respuesta rapida
+''' menymp jun 2021
+''' </summary>
+
+Public Class QRQCConnector
+
+    Dim _FilePath As String
+    Dim _LocalScopeFilePath As String
+
+    ''' <summary>
+    ''' Obtiene o establece la ruta de trabajo para los eventos de QRQC
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property FilePath
+        Get
+            Return _FilePath
+        End Get
+        Set(value)
+            _FilePath = value
+        End Set
+    End Property
+    ''' <summary>
+    ''' Obtiene o establece la ruta de trabajo para los eventos de QRQC generados recientemente
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property LocalScopeFilePath
+        Get
+            Return _LocalScopeFilePath
+        End Get
+        Set(value)
+            _LocalScopeFilePath = value
+        End Set
+    End Property
+
+    ''' <summary>
+    ''' Crea una nueva instancia de la clase QRQC conector
+    ''' </summary>
+    Public Sub New()
+
+    End Sub
+
+    ''' <summary>
+    ''' Crea una nueva instancia de un evento QRQC con la informacion necesaria para el procesamiento
+    ''' </summary>
+    ''' <returns>TRUE si se ha creado la instancia correctamente</returns>
+    Public Function CreateNewQRQC(QRQCEventInfo As QRQCDataContainer) As Boolean
+        Try
+            Dim FileData As String = File.ReadAllText(_FilePath)
+            'busca el numero de ocurrencias 
+            Dim Counter As Integer = fnStrCnt(FileData, "[" & QRQCEventInfo.SerialNumber & "]")
+            QRQCEventInfo.OcurrenceCase = Counter
+            FileData &= QRQCEventInfo.ToString
+            File.WriteAllText(_FilePath, FileData)
+
+            'escribimos el qrqc en el contexto local
+            Dim LocalFileData As String = File.ReadAllText(_LocalScopeFilePath)
+            LocalFileData &= QRQCEventInfo.ToString
+            File.WriteAllText(_LocalScopeFilePath, LocalFileData)
+            Return True
+        Catch
+            Return False
+        End Try
+    End Function
+    ''' <summary>
+    ''' Cierra una instancia existente de QRQC
+    ''' </summary>
+    ''' <param name="QRQCEventInfo"></param>
+    ''' <returns>TRUE si se realizo correctamente</returns>
+    Public Function CloseQRQC(QRQCEventInfo As QRQCDataContainer)
+        Try
+            Dim LocalFileData As String = File.ReadAllText(_LocalScopeFilePath)
+            Dim RemovedData = LocalFileData.Replace(QRQCEventInfo.ToString, "")
+            File.WriteAllText(_LocalScopeFilePath, RemovedData)
+
+            Dim FileData As String = File.ReadAllText(_FilePath)
+            RemovedData = FileData.Replace(QRQCEventInfo.ToString, "")
+            File.WriteAllText(_FilePath, RemovedData)
+            Return True
+        Catch
+            Return False
+        End Try
+    End Function
+
+    ''' <summary>
+    ''' Cuenta la cantidad de ocurrencias de una sub cadena en un string
+    ''' </summary>
+    ''' <param name="str"></param>
+    ''' <param name="substr"></param>
+    ''' <returns></returns>
+    Private Function fnStrCnt(ByVal str As String, ByVal substr As String) As Integer
+        fnStrCnt = UBound(Split(LCase(str), substr))
+    End Function
+End Class
---
+++ QRQCLibrary/QRQCDataContainer.vb
@@ -0,0 +1,74 @@
+﻿''' <summary>
+''' Contenedor de datos para un evento de QRQC
+''' </summary>
+
+Public Class QRQCDataContainer
+    ''' <summary>
+    ''' numero serial bajo el cual se bloqueara los eventos relacionados
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property SerialNumber As String
+    ''' <summary>
+    ''' Fecha de acontecimiento del evento
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property DateEvent As String
+    ''' <summary>
+    ''' Razon 1
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Reason1 As String
+    ''' <summary>
+    ''' Razon 2
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Reason2 As String
+    ''' <summary>
+    ''' Razon 3
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Reason3 As String
+    ''' <summary>
+    ''' Razon 4
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Reason4 As String
+    ''' <summary>
+    ''' Razon 5
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Reason5 As String
+    ''' <summary>
+    ''' Accion correctiva
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property CorrectiveAction As String
+    ''' <summary>
+    ''' Accion de contencion
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property ContentionAction As String
+    ''' <summary>
+    ''' numero de identificador de evento, en caso de existir varios problemas
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property OcurrenceCase As Integer
+
+    Public Overrides Function ToString() As String
+        Dim stringRepresentation As String = ""
+
+        stringRepresentation &= vbCrLf
+        stringRepresentation &= "[" & SerialNumber & "]" & vbCrLf
+        stringRepresentation &= "OcurrenceCase=" & OcurrenceCase.ToString & vbCrLf
+        stringRepresentation &= "DateEvent=" & DateEvent & vbCrLf
+        stringRepresentation &= "Reason1=" & Reason1 & vbCrLf
+        stringRepresentation &= "Reason2=" & Reason2 & vbCrLf
+        stringRepresentation &= "Reason3=" & Reason3 & vbCrLf
+        stringRepresentation &= "Reason4=" & Reason4 & vbCrLf
+        stringRepresentation &= "Reason5=" & Reason5 & vbCrLf
+        stringRepresentation &= "CorrectiveAction=" & CorrectiveAction & vbCrLf
+        stringRepresentation &= "ContentionAction=" & ContentionAction & vbCrLf
+
+        Return stringRepresentation
+    End Function
+End Class
